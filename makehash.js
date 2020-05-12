62d001ec7b9e8e7e5d85535b7624bceed3ba9316
kremnev8 <kremnev8super@yandex.ru>
2020-05-12 19:24:04 +0300

    Fixed makehash.js

--- makehash.js
+++ makehash.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
 const util = require('./build/utilites.js');
 const path = require('path');
+require('dotenv').config();
 
 const configPath = path.join(__dirname, 'config');
 const srcPath = path.join(__dirname, 'build');
-const config = util.loadJson(`${configPath}\\config`);
+
+let configName = 'config';
+if (process.env.NODE_ENV == "production") configName = 'production';
+
+const config = util.loadJson(path.join(configPath, configName));
 
 const modulesPath = path.join(srcPath, config.moduleFolder);
 
 const commandFiles = fs.readdirSync(modulesPath).filter(file => file.endsWith('.js'));
 const exeFiles = fs.readdirSync(srcPath).filter(file => file.endsWith('.js'));
-let hashes = util.loadJson(`${configPath}\\filehashes`);
+let hashes = util.loadJson(path.join(configPath, 'filehashes'));
 
 for (const file of commandFiles) {
-    let hash = util.hash(`${modulesPath}\\${file}`);
+    let hash = util.hash(path.join(modulesPath, file));
     hashes[file] = hash;
 }
 
 for (const file of exeFiles) {
-    let hash = util.hash(`${srcPath}\\${file}`);
+    let hash = util.hash(path.join(srcPath, file));
     hashes[file] = hash;
 }
 
@@ -26,4 +31,4 @@ let ver = "version_stable"; // require('os').platform() == "win32" ? "version_ex
 
 hashes[ver] = hashes[ver] + 1;
 
-util.saveJson(`${configPath}\\filehashes`, hashes);
+util.saveJson(path.join(configPath, 'filehashes'), hashes);
